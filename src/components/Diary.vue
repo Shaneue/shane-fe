39d59a22d1de1e9fe677ef9c8827a42aac8ace49
shane <shane@shaneu.cn>
2020-01-18 16:57:59 +0800

    update diary

--- src/components/Diary.vue
+++ src/components/Diary.vue
@@ -5,7 +5,7 @@
         <template v-slot:dateCell="{date, data}">
           <p
             :class="data.isSelected ? 'is-selected' : ''"
-          >{{ date.getDate() }} {{ handleData(data) ?'😊': '' }} {{ data.isSelected ? handleClick(data) : '' }}</p>
+          >{{ date.getDate() }} {{ handleData(data) ?'😊': '' }} {{ picked(data) }}</p>
         </template>
       </el-calendar>
     </el-aside>
@@ -23,7 +23,45 @@
             <el-table-column align="center" fixed :formatter="cellFormatter" label="DATE"></el-table-column>
           </el-table>
         </el-aside>
-        <el-main></el-main>
+        <el-main>
+          <el-page-header
+            :content="diary.date===null?'PLEASE PICK A DAY':(diary.date.year+'-'+diary.date.month+'-'+diary.date.date)"
+            @back="clean"
+            title="CLEAN"
+          ></el-page-header>
+          <el-form :model="diary" style="margin-top:20px">
+            <el-form-item label="SUMMARY" prop="extend" :label-width="formLabelWidth">
+              <el-input type="textarea" autosize v-model="diary.extend"></el-input>
+            </el-form-item>
+            <el-form-item
+              :label-width="formLabelWidth"
+              v-for="(item, index) in diary.items"
+              :label="'ITEM' + index"
+              :key="index"
+              :prop="'items.' + index +'.content'"
+              :rules="{required: true, message: 'NO EMPTY'}"
+            >
+              <el-input
+                v-model="item.start"
+                onkeyup="this.value=this.value.replace(/[^\d.]/g,'');"
+                maxlength="2"
+                class="input_time"
+              ></el-input>
+              <el-input
+                v-model="item.end"
+                onkeyup="this.value=this.value.replace(/[^\d.]/g,'');"
+                maxlength="2"
+                class="input_time"
+              ></el-input>
+              <el-input v-model="item.content" class="input_ext"></el-input>
+              <el-button size="small" @click.prevent="removeItem(item)">DEL</el-button>
+            </el-form-item>
+          </el-form>
+          <span>
+            <el-button @click="addItem">NEW ITEM</el-button>
+            <el-button type="primary" @click.prevent="update">UPDATE</el-button>
+          </span>
+        </el-main>
       </el-container>
     </el-main>
   </el-container>
@@ -34,11 +72,59 @@ export default {
   data() {
     return {
       value: new Date(),
-      tableData: []
+      tableData: [],
+      formLabelWidth: '120px',
+      diary: {
+        extend: '',
+        items: [],
+        date: null
+      }
     }
   },
   methods: {
+    clean() {
+      this.diary.extend = ''
+      this.diary.items = []
+    },
+    update() {
+      this.$axios({
+        method: 'post',
+        url: 'diary',
+        data: this.diary
+      }).then(response => {
+        if (response.data.status === 0) {
+          this.$global.success(response.data.message)
+          this.init()
+        } else {
+          this.$global.error(response.data.message)
+        }
+      }).catch((error) => {
+        this.$global.error(error)
+      })
+    },
+    removeItem(item) {
+      var index = this.diary.items.indexOf(item)
+      if (index !== -1) {
+        this.diary.items.splice(index, 1)
+      }
+    },
+    addItem() {
+      this.diary.items.push({ start: '', end: '', content: '' })
+    },
+    picked(data) {
+      if (data.isSelected) {
+        return this.handleClick(data)
+      } else {
+        return ''
+      }
+    },
     handleClick(data) {
+      let splits = data.day.split('-')
+      this.diary.date = {
+        year: splits[0],
+        month: splits[1],
+        date: splits[2]
+      }
       return '✔️'
     },
     handleData(data) {
@@ -49,21 +135,43 @@ export default {
     },
     tableClick(row) {
       this.value = new Date(row)
+      let splits = row.split('-')
+      this.diary.date = {
+        year: splits[0],
+        month: splits[1],
+        date: splits[2]
+      }
+      this.$axios({
+        method: 'get',
+        url: 'diary/' + splits[0] + '/' + splits[1] + '/' + splits[2]
+      }).then(response => {
+        if (response.data.status === 0) {
+          this.diary = response.data.data
+        } else {
+          this.$global.error(response.data.message)
+        }
+      }).catch((error) => {
+        this.$global.error(error)
+      })
+    },
+    init() {
+      this.$axios({
+        method: 'get',
+        url: 'diary'
+      }).then(response => {
+        if (response.data.status === 0) {
+          this.tableData = []
+          this.tableData.push(...response.data.data)
+        } else {
+          this.$global.error(response.data.message)
+        }
+      }).catch((error) => {
+        this.$global.error(error)
+      })
     }
   },
   created() {
-    this.$axios({
-      method: 'get',
-      url: 'diary'
-    }).then(response => {
-      if (response.data.status === 0) {
-        this.tableData.push(...response.data.data)
-      } else {
-        this.$global.error(response.data.message)
-      }
-    }).catch((error) => {
-      this.$global.error(error)
-    })
+    this.init()
   },
   mounted() {
   }
@@ -74,5 +182,13 @@ export default {
 <style scoped>
 h1 {
   font-weight: normal;
+}
+
+.input_time {
+  width: 10%;
+}
+
+.input_ext {
+  width: 60%;
 }
 </style>
